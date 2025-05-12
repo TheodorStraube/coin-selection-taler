2e945f41fb9259d868ff52e6cb4c4bff6a42fe6c
Theodor Straube <theodor.straube@mailbox.tu-dresden.de>
2025-05-12 11:37:01 +0200

    stand

--- coin-selection-c-master/run.sh
+++ coin-selection-c-master/run.sh
@@ -1,4 +1,15 @@
 export PYTHONPATH="../python"
+numfiles=$(ls -l simulation/users | grep -v ^d | wc -l)
 cd build
-./coin_selection_c
+
+if [ $1 ] && (( $1==-1 ));
+then
+    echo "true"
+else
+    echo "false"
+fi
+
+exit
+
+./coin_selection_c "$@"
 cd ..
