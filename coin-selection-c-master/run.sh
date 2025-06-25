f1d3c8b7227a77d66d9919bf844f3726403fd0c7
Theodor Straube <theodor.straube@mailbox.tu-dresden.de>
2025-06-25 13:09:57 +0200

    reenable run.sh

--- coin-selection-c-master/run.sh
+++ coin-selection-c-master/run.sh
@@ -2,14 +2,12 @@ export PYTHONPATH="../python"
 numfiles=$(ls -l simulation/users | grep -v ^d | wc -l)
 cd build
 
+./coin_selection_c "$@"
+cd ..
+
 if [ $1 ] && (( $1==-1 ));
 then
     echo "true"
 else
     echo "false"
 fi
-
-exit
-
-./coin_selection_c "$@"
-cd ..
